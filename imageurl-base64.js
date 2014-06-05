65e38f32aa20360fed902a977d55b0253fe12791
Lee Crossley <leee@hotmail.co.uk>
2014-06-05 20:20:31 +0100

    Minor update

--- imageurl-base64.js
+++ imageurl-base64.js
@@ -1,6 +1,6 @@
 var request = require("request");
 
-exports.imgUrlToBase64 = function (url, callback) {
+exports = function (url, callback) {
     var options = {
         uri: url,
         encoding: "binary"
@@ -10,9 +10,8 @@ exports.imgUrlToBase64 = function (url, callback) {
             var prefix = "data:" + resp.headers["content-type"] + ";base64,";
             var img = new Buffer(body.toString(), "binary").toString("base64");
             return callback(prefix + img);
-        } else {
-            console.log(e);
-            return callback();
         }
+        console.log(e);
+        return callback();
     });
 };
