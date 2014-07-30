9abc1c511265ca709d3838f730d98ca17aa6402f
Lee Crossley <leee@hotmail.co.uk>
2014-07-30 20:28:42 +0100

    Bump version and use strict

--- imageurl-base64.js
+++ imageurl-base64.js
@@ -1,17 +1,20 @@
 var request = require("request");
 
 var i2b = function (url, callback) {
+    "use strict";
+
     var options = {
         uri: url,
         encoding: "binary"
     };
+
     return request(options, function(e, resp, body) {
         if (!e && resp.statusCode === 200) {
             var prefix = "data:" + resp.headers["content-type"] + ";base64,";
             var img = new Buffer(body.toString(), "binary").toString("base64");
             return callback(prefix + img);
         }
-        console.log(e);
+
         return callback();
     });
 };
